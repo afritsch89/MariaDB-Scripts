591bc18f575a80484f0d89a00d2a4e69cc7d124d
afritsch89 <afritsch89@gmail.com>
2023-03-24 06:43:52 -0700

    Update performance.sh

--- performance.sh
+++ performance.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+# Set timestamp format
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
 # Connect to MariaDB and run queries
+echo "=======================" >> performance.log
+echo "== Query Performance ==" >> performance.log
+echo "=======================" >> performance.log
+echo "Timestamp: $timestamp" >> performance.log
 mysql -u username -p'password' -e "SELECT digest_text, SUM(executions) as total_executions, SUM(sum_timer_wait) as total_execution_time, AVG(sum_timer_wait) as avg_execution_time FROM performance_schema.events_statements_summary_by_digest WHERE digest_text IS NOT NULL GROUP BY digest_text ORDER BY total_execution_time DESC;" >> performance.log
 
+echo "====================" >> performance.log
+echo "== Resource Usage ==" >> performance.log
+echo "====================" >> performance.log
+echo "Timestamp: $timestamp" >> performance.log
 mysql -u username -p'password' -e "SELECT stage, SUM(timer_wait) as total_cpu_time FROM performance_schema.events_stages_summary_global_by_event_name WHERE event_name LIKE 'stage/sql/%' GROUP BY stage;" >> performance.log
 
+echo "==========================" >> performance.log
+echo "== Locking and Blocking ==" >> performance.log
+echo "==========================" >> performance.log
+echo "Timestamp: $timestamp" >> performance.log
 mysql -u username -p'password' -e "SELECT table_name, COUNT(*) as num_waits, SUM(timer_wait) as total_wait_time FROM performance_schema.data_lock_waits GROUP BY table_name ORDER BY total_wait_time DESC;" >> performance.log
